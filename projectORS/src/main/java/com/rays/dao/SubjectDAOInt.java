5c6febbb2153489cf1c2ea875d37e0cd53d60362
Abhishek Patidar <abhishek2002.ap@gmail.com>
2022-08-04 00:02:27 +0530

    ORS

---
+++ projectORS/src/main/java/com/rays/dao/SubjectDAOInt.java
@@ -0,0 +1,13 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.SubjectDTO;
+
+/**
+ * Subject DAO Interface
+ * @author Abhishek Patidar
+ *
+ */
+public interface SubjectDAOInt extends BaseDAOInt<SubjectDTO>{
+
+}
